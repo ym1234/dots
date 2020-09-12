41ca0da9dc81f23644ff96441f64f4abe417436f
ym555 <mohamed.yoyo100@gmail.com>
2020-09-12 13:41:23 +0200

    updates and stuff

--- .config/mpv/scripts/anki-create.lua
+++ .config/mpv/scripts/anki-create.lua
@@ -32,72 +32,89 @@ function set_end()
 end
 
 function get_source()
-	local file, external, track_no, audio_track_no
+	local sub_file, sub_external, sub_track_no, audio_track_no
 	for i, k in ipairs(mp.get_property_native('track-list')) do
-		if k.type == 'sub' and k.selected == true and file == nil then
+		if audio_track_no ~= nil and sub_external ~= nil then
+			break
+		end
+
+		if k.type == 'sub' and k.selected == true then
 			if k.external == true then
-				file = k["external-filename"]
-				external = true
-				track_no = 0
+				sub_file = k["external-filename"]
+				sub_external = true
+				sub_track_no = 0
 			else
-				external = false
-				track_no = k["src-id"] - 1 -- TODO fix this
-			end
-		else
-			if k.type == 'audio' and k.selected == true then
-			audio_track_no = k["src-id"] - 1 -- TODO fix this
+				sub_external = false
+				sub_track_no = k["src-id"] - 1 -- TODO fix this
 			end
 		end
+		if k.type == 'audio' and k.selected == true then
+			audio_track_no = k["src-id"] - 1 -- TODO fix this
+		end
 	end
-	return mp.get_property_native('filename'), file, external, track_no, audio_track_no
+	return mp.get_property_native('filename'), sub_file, sub_external, sub_track_no, audio_track_no
 end
 
-function my_popen(arg)
+function my_popen(arg, sep)
 	local file = io.popen(arg)
-	local output = file:read('*all')
+	local out = ""
+	for line in file:lines() do
+		out = out .. line .. sep
+	end
 	file:close()
+	return out
 end
 
 function create_card()
 	local video_file, subtitle_file, external, track_no, audio_track_no = get_source()
 	print(video_file, subtitle_file, external, track_no, audio_track_no)
-	local f = nil
+
+	local cmd = '~/bin/extract-dialogue' .. ' -i ' .. ("%q"):format(video_file) .. '  -a ' .. audio_track_no .. ' -k ' .. (start_time * 1000) .. ' -e ' .. (end_time  * 1000) .. ' -s '
 	if external then
-		print('~/bin/extract-dialogue' .. ' -i ' .. ("%q"):format(video_file) .. ' -s ' .. ("%q"):format(subtitle_file) ..  '  -a ' .. audio_track_no .. ' -k ' .. math.floor(start_time * 1000) .. ' -e ' .. math.ceil(end_time  * 1000))
-		f = io.popen('~/bin/extract-dialogue' .. ' -i ' .. ("%q"):format(video_file) .. ' -s ' .. ("%q"):format(subtitle_file) ..  '  -a ' .. audio_track_no .. ' -k ' .. (start_time * 1000) .. ' -e ' .. (end_time  * 1000))
+		cmd = cmd .. ("%q"):format(subtitle_file)
 	else
-		-- TODO(ym): NOT TESTED
-		f = io.popen('~/bin/extract-dialogue' .. ' -i \'' .. video_file .. '\' -s ' .. track_no ..  '  -a ' .. audio_track_no .. ' -k ' .. (start_time * 1000) .. ' -e ' .. (end_time  * 1000))
+		cmd = cmd .. track_no
 	end
-	local subs = ""
-	for line in f:lines() do
-		subs = subs .. line .. "<br>"
-	end
-	f:close()
+	print(cmd)
+
+	subs = my_popen(cmd, "<br>")
 	print(subs)
+
 	local new_name = "mpvcreate" .. os.time() .. ".mp3"
-	io.popen('mv output.mp3 \'' .. collection .. new_name .. '\'') -- seriously?
-	info = {
-		action = "addNote",
+	my_popen('mv output.mp3 \'' .. collection .. new_name .. '\'', "") -- seriously?
+
+	req  = {
+		action = "findNotes",
+		version = 6,
+		params = { query = "\"deck:" .. deck_name .. "\" added:1 is:new" }
+	}
+	resp, _, _ = http.request('http://127.0.0.1:8765/', json.encode(req))
+	val = json.decode(resp)
+	table.sort(val.result)
+
+	req = {
+		action = "updateNoteFields",
 		version = 6,
 		params = {
 			note = {
-				deckName = deck_name,
-				modelName = model_name,
+				id = val.result[#val.result],
 				fields = {
-					[word_field] = subs,
 					[sentence_field] = subs,
 					[audio_field] = "[sound:" .. new_name .. "]",
-				},
-				tags =  {
-					"mpv-create",
-					"日本語"
 				},
 			}
 		}
 	}
-	b, c, h = http.request('http://127.0.0.1:8765/', json.encode(info))
+	print(json.encode(req))
+	b, _, _ = http.request('http://127.0.0.1:8765/', json.encode(req))
 	print(b)
+
+	req = {
+		action = "addTags",
+		version = 6,
+		params = { notes = { val.result[#val.result] }, tags = "mpv-create" }
+	}
+	http.request('http://127.0.0.1:8765/', json.encode(req))
 end
 
 
@@ -105,9 +122,9 @@ end
 function save_clip()
 	local video_file, subtitle_file, _, _, _ = get_source()
 	local new_name = "mpv-clip-" .. os.time() .. ".mp4"
-	my_popen('ln -s ' .. ("%q"):format(subtitle_file) .. ' sub_file')
-	my_popen('ffmpeg -v fatal -ss ' .. start_time  .. ' -to ' .. end_time .. " -copyts -i " .. ("%q"):format(video_file)  .. " -vf subtitles=sub_file -reset_timestamps 1 " .. new_name)
-	my_popen('rm sub_file')
+	my_popen('ln -s ' .. ("%q"):format(subtitle_file) .. ' sub_file', "")
+	my_popen('ffmpeg -v fatal -ss ' .. start_time  .. ' -to ' .. end_time .. " -copyts -i " .. ("%q"):format(video_file)  .. " -vf subtitles=sub_file -reset_timestamps 1 " .. new_name, "")
+	my_popen('rm sub_file', "")
 	mp.osd_message('Clip: ' .. new_name)
 end
 
